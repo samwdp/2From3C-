01538316aa870e9892113a79fbb86c020dbcdef9
samwdp <samwdp@gmail.com>
2016-05-05 18:16:15 +0100

    complete

--- Dice/Form1.Designer.cs
+++ Dice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dice
 {
-    partial class Form1
+    partial class Dice
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dice));
+            this.randomBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.number3 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // randomBtn
+            // 
+            this.randomBtn.Location = new System.Drawing.Point(198, 38);
+            this.randomBtn.Name = "randomBtn";
+            this.randomBtn.Size = new System.Drawing.Size(116, 23);
+            this.randomBtn.TabIndex = 1;
+            this.randomBtn.Text = "Random Number";
+            this.randomBtn.UseVisualStyleBackColor = true;
+            this.randomBtn.Click += new System.EventHandler(this.randomBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(221, 90);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(39, 41);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(100, 20);
+            this.number1.TabIndex = 3;
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(39, 67);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(100, 20);
+            this.number2.TabIndex = 4;
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(39, 93);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(100, 20);
+            this.number3.TabIndex = 5;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(39, 154);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(275, 96);
+            this.output.TabIndex = 6;
+            this.output.Text = "";
+            // 
+            // Dice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(362, 262);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.randomBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Dice";
+            this.Text = "2 From 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button randomBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.TextBox number3;
+        private System.Windows.Forms.RichTextBox output;
     }
 }
 
--- Dice/Form1.cs
+++ Dice/Form1.cs
@@ -10,11 +10,161 @@ using System.Windows.Forms;
 
 namespace Dice
 {
-    public partial class Form1 : Form
+    public partial class Dice : Form
     {
-        public Form1()
+        bool isEven = false;
+
+        public Dice()
         {
             InitializeComponent();
+        }
+
+        private void randomBtn_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            number1.Text = r.Next(1, 6).ToString();
+            number2.Text = r.Next(1, 6).ToString();
+            number3.Text = r.Next(1, 6).ToString();
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+            int n1 = Convert.ToInt16(number1.Text);
+            int n2 = Convert.ToInt16(number2.Text);
+            int n3 = Convert.ToInt16(number3.Text);
+
+            int dice1 = getDice1(n1, n2, n3);
+            int dice2 = getDice2(dice1, n1, n2, n3);
+
+            output.AppendText("Dice 1: " + dice1.ToString() + "\n" + "Dice 2: " + dice2.ToString());
+        }
+
+        public int getDice1(int number1, int number2, int number3)
+        {
+            int i = (number1 + number2 + number3) % 6;
+            if (i == 0)
+            {
+                return 6;
+            }
+            else {
+                return i;
+            }
+        }
+
+        public int getDice2(int dice1, int number1, int number2, int number3)
+        {
+            int temp1 = number1;
+            int temp2 = number2;
+            int temp3 = number3;
+            int tempDice = dice1 % 2;
+
+            //if even do things
+            if (tempDice == 0)
+            {
+                isEven = true;
+                temp1 = 7 - number1;
+                temp2 = 7 - number2;
+                temp3 = 7 - number3;
+            }
+
+            //check if 2 different even numbers
+            if (temp1 == 2 || temp1 == 4 || temp1 == 6)
+            {
+                if (temp2 == 2 || temp2 == 4 || temp2 == 6)
+                {
+                    return temp3;
+                }
+                else if (temp3 == 2 || temp3 == 4 || temp3 == 6)
+                {
+                    return temp2;
+                }
+
+            }
+            else if (temp2 == 2 || temp2 == 4 || temp2 == 6)
+            {
+                if (temp3 == 2 || temp3 == 4 || temp3 == 6)
+                {
+                    return temp1;
+                }
+            }
+            else if (dice1 == 3 || dice1 == 6)
+            {
+                int i = 0;
+                if (temp1 == 2 && temp2 == 2 && temp3 == 5)
+                {
+                    return edgeCase();
+
+                }
+                else {
+                    i = calculateDifferent(temp1, temp2, temp3);
+                }
+
+                if (temp1 == 2 && temp2 == 5 && temp3 == 2)
+                {
+                    return edgeCase();
+                }
+                else {
+                    i = calculateDifferent(temp1, temp2, temp3);
+                }
+
+                if (temp1 == 5 && temp2 == 2 && temp3 == 2)
+                {
+                    return edgeCase();
+                }
+                else {
+                    i = calculateDifferent(temp1, temp2, temp3);
+                }
+
+                return i * 2;
+
+            }
+            else if (number1 == number2)
+            {
+                return temp3 + 1;
+            }
+            else if (number1 == number3)
+            {
+                return temp2 + 1;
+            }
+            else if (number2 == number3)
+            {
+                return temp1 + 1;
+            }
+
+            return 0;
+
+        }
+
+        public int calculateDifferent(int i1, int i2, int i3)
+        {
+            int counter = 0;
+            if (i1 != i2)
+            {
+                counter += 1;
+            }
+            if (i1 != i3)
+            {
+                counter += 1;
+            }
+            if (i2 != i3)
+            {
+                counter += 1;
+            }
+
+            return counter + 1;
+        }
+
+        public int edgeCase()
+        {
+            if (isEven)
+            {
+                return 5;
+            }
+            else {
+                return 2;
+            }
         }
     }
 }
